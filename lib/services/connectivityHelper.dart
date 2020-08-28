ef925318dd22667ca282d08d96048c67d1f01d57
Dominik Vagala <d.vagala@gmail.com>
2020-08-28 15:25:27 +0200

    fix onNetworkChanged miss first event

--- lib/services/connectivityHelper.dart
+++ lib/services/connectivityHelper.dart
@@ -10,11 +10,8 @@ class ConnectivityHelper {
   ConnectivityResult previousConnection;
 
   void onConnectionAvailable(Null Function() callback) {
-    // When the app is loaded onConnectivityChanged is fired, so skip first event
-    // from the stream. This is probably not optimal solution.
     connectionChangedSubscription = Connectivity()
         .onConnectivityChanged
-        .skip(1)
         .listen((ConnectivityResult result) {
       if (result != ConnectivityResult.none &&
           previousConnection == ConnectivityResult.none) {
